50474fcf03a04d9032b74ede9155f441777b2693
209410108h <hank5947887@gmail.com>
2021-10-07 22:07:19 +0800

    first commit

---
+++ src/App_08.js
@@ -0,0 +1,15 @@
+import Todo_08 from './components/Todo_08';
+import './App.css';
+
+function App_08() {
+  return (
+    <div >
+      <h1>My Todos</h1>
+      <Todo_08 text='Learn Python'/>
+      <Todo_08 text='Learn SQL'/>
+      <Todo_08 text='Learn Android Studio'/>
+</div>
+  );
+}
+
+export default App_08;
